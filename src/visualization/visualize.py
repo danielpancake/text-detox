9d31b698e904d02da9d337a3495a38201eba64b9
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2023-11-05 15:02:05 +0300

    Added final metrics

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -31,6 +31,52 @@ def generate_dataset_text_lengths_distribution():
     plt.close()
 
 
+def analyze_scores():
+    df_delete_scores = pd.read_csv(
+        os.path.join(FILEPATH, "../../data/interim/delete_baseline_scores.csv"),
+    )
+    df_replace_scores = pd.read_csv(
+        os.path.join(FILEPATH, "../../data/interim/replace_baseline_scores.csv"),
+    )
+    df_detoxGPT_scores = pd.read_csv(
+        os.path.join(FILEPATH, "../../data/interim/detoxGPT_scores.csv"),
+    )
+
+    # Count mean scores for each model
+    delete_mean = df_delete_scores.mean(axis=0)
+    replace_mean = df_replace_scores.mean(axis=0)
+    detoxGPT_mean = df_detoxGPT_scores.mean(axis=0)
+
+    df_total = pd.concat([delete_mean, replace_mean, detoxGPT_mean], axis=1)
+    df_total.columns = ["delete", "replace", "detoxGPT"]
+
+    # Plot the scores
+    df_total.plot.bar()
+    plt.title("Scores for different models")
+    plt.xlabel("Score")
+    plt.ylabel("Value")
+
+    # Draw numbers above bars
+    for idx, model in enumerate(df_total.columns):
+        for index, value in enumerate(df_total[model]):
+            plt.text(
+                index + idx / 4 - 0.3,
+                value + 0.01,
+                str(round(value, 2)),
+                color="black",
+                fontweight="bold",
+            )
+
+    # Rotate x-axis labels
+    plt.xticks(rotation=0)
+
+    plt.savefig(
+        os.path.join(FILEPATH, "../../reports/figures/scores_for_different_models.png"),
+        format="png",
+    )
+
+
 if __name__ == "__main__":
     generate_dataset_text_lengths_distribution()
+    analyze_scores()
     print("Done!")
